cab53123a57994c99b77b9442fa655f554a82076
Jason Lewis <jason.lewis1991@gmail.com>
2016-04-29 12:10:41 +0930

    Send different kinds of responses.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('redirect', function () {
+    return redirect('done');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('done', function () {
+    return 'redirection complete';
+});
+
+Route::get('json', function () {
+    return response()->json(['name' => 'jason', 'age' => 25]);
+});
+
+Route::get('file', function () {
+    return response()->file(storage_path('files/instructions.txt'));
+});
+
+Route::get('download', function () {
+    return response()->download(storage_path('files/instructions.txt'));
+});
+
+Route::get('error', function () {
+    abort(403);
 });
