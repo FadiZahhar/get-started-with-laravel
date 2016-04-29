5064e8c6496f2d179330cc5ec7805ee7467ec554
Jason Lewis <jason.lewis1991@gmail.com>
2016-04-29 11:58:38 +0930

    Responding with HTML from routes.

---
+++ resources/views/layout.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Application</title>
+
+    </head>
+    <body>
+
+        <h2>Laravel Application</h2>
+
+        <hr>
+
+        <a href="#">Home</a> |
+        <a href="#">Contact</a> |
+        <a href="#">About</a>
+
+        <hr>
+
+        <?php echo $content; ?>
+
+    </body>
+</html>
